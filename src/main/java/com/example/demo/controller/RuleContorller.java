200fab1c7a7d87b575ac1bb5331254530e0efb80
songxl <337946745@qq.com>
2022-10-13 14:19:32 +0800

    easyRule 代码调整

--- src/main/java/com/example/demo/controller/RuleContorller.java
+++ src/main/java/com/example/demo/controller/RuleContorller.java
@@ -5,7 +5,10 @@ import com.example.demo.facade.RuleFacade;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 /**
@@ -21,8 +24,6 @@ import org.springframework.web.bind.annotation.*;
 public class RuleContorller {
     @Autowired
     RuleFacade testFacade;
-
-
 
     @PostMapping("/execute")
     @ApiOperation("规则执行")
